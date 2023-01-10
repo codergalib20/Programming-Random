ba5c9c40e80fdad52c04563adf2fc9c6cb45243c
codergalib20 <coderboygalif@gmail.com>
2023-01-10 20:33:01 +0600

    commited

---
+++ cpp/input-output.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    cout << "Welcome to My first programm" << endl;
+
+    string str = "s";
+
+    // cin >> str;
+
+    cout << srt;
+}
