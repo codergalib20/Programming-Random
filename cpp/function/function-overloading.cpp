7b3b38955828e75bdd21ad5f27b4d1dd73a34384
codergalib20 <coderboygalif@gmail.com>
2023-02-02 01:28:33 +0600

    learn more about function overloading in c++

--- cpp/function/function-overloading.cpp
+++ cpp/function/function-overloading.cpp
@@ -3,13 +3,35 @@
 using namespace std;
 
 // same name different argements
-int test() {}
-int test(int a) {}
-float test(double a) {}
-int test(int a, double b) {}
+// int test() {}
+// int test(int a) {}
+// float test(double a) {}
+// int test(int a, double b) {}
+
+// function with float type parameter
+float absolute(float var)
+{
+    if (var < 0.0)
+        var = -var;
+    return var;
+}
+
+// function with int type parameter
+int abslute(int var)
+{
+    if (var < 0)
+    {
+        var = -var;
+    }
+    return var;
+}
 
 int main()
 {
+    // call function with int type parameter
+    cout << "Absolute value of -5 = " << absolute(-5) << endl;
 
+    // call function with float type parameter
+    cout << "Absolute value of 5.5 = " << absolute(4.4f) << endl;
     return 0;
 }
