d260a608128d143b30fdb4855e147e810cfd2049
codergalib20 <coderboygalif@gmail.com>
2023-02-04 22:14:53 +0600

    commited

---
+++ cpp/array/first.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int count = 0;
+    // int x[3][4] = {{1, 2, 3}, {5, 6, 7, 8}, {9}};
+    // std::cout << x;
+    int y[4] = {2,3,4,5};
+    while(count <= 4) {
+        std::cout << y[count];
+        count++;
+    }
+}
