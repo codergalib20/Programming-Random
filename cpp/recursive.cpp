ab0cf29d26250a97b5ffa206065b354805f5d9da
codergalib20 <coderboygalif@gmail.com>
2023-02-04 21:56:11 +0600

    commited

---
+++ cpp/recursive.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+void greet();
+
+int main()
+{
+    greet();
+}
+
+void greet()
+{
+    cout << "*" << endl;
+    cout << "* *" << endl;
+    cout << "* * *" << endl;
+    cout << "* * * *" << endl;
+    cout << "* * * * *" << endl;
+    cout << "* * * * * *" << endl;
+    cout << "* * * * * * *" << endl;
+    cout << "* * * * * * * *" << endl;
+    cout << "* * * * * * * * *" << endl;
+    cout << "* * * * * * * * * *" << endl;
+    cout << "* * * * * * * * * * *" << endl;
+    cout << "* * * * * * * * * * * *" << endl;
+    cout << "Start coding by printing" << endl;
+}
