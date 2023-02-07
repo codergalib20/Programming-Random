1830901d85b62314a7312e2a6adc597fc5240782
codergalib20 <coderboygalif@gmail.com>
2023-02-07 16:30:28 +0600

    learning function in c

---
+++ c/function/hello-world.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void printHello () {
+     printf("Hello world");
+}
+int main () {
+   printHello();
+}
---
+++ cpp/array/number-convert-array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        // arr[i] = i;
+    }
+    cout << (arr[2] = 2) << endl;
+}
