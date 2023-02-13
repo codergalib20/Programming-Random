fb169753a5975f7f486902f41551622bc121133f
codergalib20 <coderboygalif@gmail.com>
2023-02-14 02:05:27 +0600

    Learn about (char) in c++

---
+++ cpp/char.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char language[10] = { 'E', 'n', 'g', 'l', 'i', 's', 'h' };
+    char secondLang[] = "Bangla";
+
+    cout << "First Language : " << language << endl;
+    cout << "Second Language : " << secondLang << endl;
+
+    return 0;
+}
