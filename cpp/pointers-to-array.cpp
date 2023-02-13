decc3389b38a8d90ae94db543dbbd2cd01d534a2
codergalib20 <coderboygalif@gmail.com>
2023-02-14 03:22:05 +0600

    learn more pointers to array in c++

--- cpp/pointers-to-array.cpp
+++ cpp/pointers-to-array.cpp
@@ -2,12 +2,23 @@
 
 using namespace std;
 
-int main () {
+int main()
+{
     float arr[5];
     float *ptr;
 
-    for(int i = 0; i < 5; ++i) {
-        cout << "&arr[ " << i << "] = " << &arr[i] << endl; 
+    for (int i = 0; i < 5; ++i)
+    {
+        cout << "&arr[ " << i << "] = " << &arr[i] << endl;
+    }
+
+    ptr = arr;
+
+    cout << "\nDisplaying memory address using pointers: " << endl;
+
+    for (int i = 0; i < 5; ++i)
+    {
+        cout << "ptr + " << i << " = " << ptr + i << endl;
     }
 
     return 0;
