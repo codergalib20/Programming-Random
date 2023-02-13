22a958767ce5f3b6a415b3b9872a78678e11e962
codergalib20 <coderboygalif@gmail.com>
2023-02-14 02:33:25 +0600

    learning pointers

---
+++ cpp/pointers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int var1 = 5;
+    float var2 = 17.28;
+    string var3 = "I love seafood";
+
+    cout << "Adress of var1: " << &var1 << endl;
+    cout << "Adress of var2: " << &var2 << endl;
+    cout << "Adress of var3: " << &var3 << endl;
+    return 0;
+}
