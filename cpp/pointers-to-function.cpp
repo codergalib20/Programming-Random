7e00efad4c6d5524c7ed851c6bf1e8a009e6a8bc
codergalib20 <coderboygalif@gmail.com>
2023-02-14 03:09:33 +0600

    swaping funcion using c++ function by Pointers

--- cpp/pointers-to-function.cpp
+++ cpp/pointers-to-function.cpp
@@ -2,7 +2,8 @@
 
 using namespace std;
 
-void numSwap(int &num1, int &num2);
+// void numSwap(int &num1, int &num2);
+void numSwap(int *, int *);
 
 int main()
 {
@@ -14,17 +15,26 @@ int main()
     cout << "Y = " << y << endl;
 
     // call Swap function
-    numSwap(x, y);
-
+    // numSwap(x, y); // function using dereference
+    numSwap(&x, &y); // call Swaping function by pointers
     return 0;
 }
 
-void numSwap(int &num1, int &num2)
+// void numSwap(int &num1, int &num2)
+// {
+// int temp;
+// temp = num1;
+// num1 = num2;
+// num2 = temp;
+// cout << "X = " << num1 << endl;
+// cout << "Y = " << num2 << endl;
+// }
+void numSwap(int *num1, int *num2)
 {
     int temp;
-    temp = num1;
-    num1 = num2;
-    num2 = temp;
-    cout << "X = " << num1 << endl;
-    cout << "Y = " << num2 << endl;
+    temp = *num1;
+    *num1 = *num2;
+    *num2 = temp;
+    cout << "X = " << *num1 << endl;
+    cout << "Y = " << *num2 << endl;
 }
