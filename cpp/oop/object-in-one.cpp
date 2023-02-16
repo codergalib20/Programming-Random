295e67b5fb0c6df3bb217fe185bc9015297ee9de
codergalib20 <coderboygalif@gmail.com>
2023-02-17 01:43:30 +0600

    Learned about first constructor in C++ OPP

--- cpp/oop/object-in-one.cpp
+++ cpp/oop/object-in-one.cpp
@@ -2,6 +2,39 @@
 
 using namespace std;
 
+// class Employee
+// {
+// public:
+//     string Name;
+//     string Company;
+//     int Age;
+
+//     void introduction()
+//     {
+//         cout << "Name - " << Name << endl;
+//         cout << "Company - " << Company << endl;
+//         cout << "Age - " << Age << endl;
+//     }
+//     void setIntroduction(string name, string company, int age)
+//     {
+//         Name = name;
+//         Company = company;
+//         Age = age;
+//     }
+// };
+
+// int main()
+// {
+//     Employee employee1;
+//     // employee1.Name = "Galib";
+//     // employee1.Company = "GBS soft";
+//     // employee1.Age = 17;
+//     //
+//     // employee1.introduction();
+//     employee1.setIntroduction("Asadullah", "GBS soft", 17);
+//     employee1.introduction();
+// }
+
 class Employee
 {
 public:
@@ -9,13 +42,15 @@ public:
     string Company;
     int Age;
 
+    // Custom function
     void introduction()
     {
         cout << "Name - " << Name << endl;
         cout << "Company - " << Company << endl;
         cout << "Age - " << Age << endl;
     }
-    void setIntroduction(string name, string company, int age)
+    // Constructor
+    Employee(string name, string company, int age)
     {
         Name = name;
         Company = company;
@@ -25,12 +60,6 @@ public:
 
 int main()
 {
-    Employee employee1;
-    // employee1.Name = "Galib";
-    // employee1.Company = "GBS soft";
-    // employee1.Age = 17;
-    //
-    // employee1.introduction();
-    employee1.setIntroduction("Asadullah", "GBS soft", 17);
+    Employee employee1 = Employee("Galib", "Edive", 17);
     employee1.introduction();
 }
