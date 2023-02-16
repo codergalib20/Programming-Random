25d9bc14e554883856184193df5a10dd69db6ac3
codergalib20 <coderboygalif@gmail.com>
2023-02-17 01:34:49 +0600

    make first class object with a get value function

--- cpp/oop/object-in-one.cpp
+++ cpp/oop/object-in-one.cpp
@@ -4,12 +4,25 @@ using namespace std;
 
 class Employee
 {
+public:
     string Name;
     string Company;
     int Age;
+
+    void introduction()
+    {
+        cout << "Name - " << Name << endl;
+        cout << "Company - " << Company << endl;
+        cout << "Age - " << Age << endl;
+    }
 };
 
 int main()
 {
     Employee employee1;
+    employee1.Name = "Galib";
+    employee1.Company = "GBS soft";
+    employee1.Age = 17;
+
+    employee1.introduction();
 }
