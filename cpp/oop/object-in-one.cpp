f3668b8e47085ee97414c24759b2ac58a283f4d5
codergalib20 <coderboygalif@gmail.com>
2023-02-17 01:38:26 +0600

    learning code and send data to class without constructor by the help of custom function

--- cpp/oop/object-in-one.cpp
+++ cpp/oop/object-in-one.cpp
@@ -15,14 +15,22 @@ public:
         cout << "Company - " << Company << endl;
         cout << "Age - " << Age << endl;
     }
+    void setIntroduction(string name, string company, int age)
+    {
+        Name = name;
+        Company = company;
+        Age = age;
+    }
 };
 
 int main()
 {
     Employee employee1;
-    employee1.Name = "Galib";
-    employee1.Company = "GBS soft";
-    employee1.Age = 17;
-
+    // employee1.Name = "Galib";
+    // employee1.Company = "GBS soft";
+    // employee1.Age = 17;
+    //
+    // employee1.introduction();
+    employee1.setIntroduction("Asadullah", "GBS soft", 17);
     employee1.introduction();
 }
