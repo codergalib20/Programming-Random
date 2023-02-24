84efab013305218dc0c6c8cb64b1657a94402c8e
codergalib20 <coderboygalif@gmail.com>
2023-02-24 22:39:58 +0600

    start learning switch

--- golang/start program/if-else.go
+++ golang/start program/if-else.go
@@ -8,13 +8,6 @@ func average(a, b, c int) float32 {
 }
 
 func main() {
-	// if condition {
-
-	// } else if condition {
-
-	// } else {
-
-	// }
 	quiz1, quiz2, quiz3 := 9, 7, 8
 
 	if quiz1 > quiz2 {
