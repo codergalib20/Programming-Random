9dc845ccc685e442f911284455673e5199aec89f
codergalib20 <coderboygalif@gmail.com>
2023-02-24 22:17:23 +0600

    condition

---
+++ golang/start program/if-else.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	// if condition {
+
+	// } else if condition {
+
+	// } else {
+
+	// }
+}
---
+++ golang/start program/operators.go
@@ -0,0 +1,7 @@
+package main
+
+// = Addition +=
+// - Substraction -=
+// * Multiplication *=
+// / Division /=
+// % Remainder %=
