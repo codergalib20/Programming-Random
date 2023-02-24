c73f7980c03fc715b6ef4ba0b6eb251a9653981c
codergalib20 <coderboygalif@gmail.com>
2023-02-24 22:29:01 +0600

    learned if else condition

--- golang/start program/if-else.go
+++ golang/start program/if-else.go
@@ -1,5 +1,12 @@
 package main
 
+import "fmt"
+
+func average(a, b, c int) float32 {
+	// Convert the sum of the scores into a float32
+	return float32(a+b+c) / 3
+}
+
 func main() {
 	// if condition {
 
@@ -8,4 +15,19 @@ func main() {
 	// } else {
 
 	// }
+	quiz1, quiz2, quiz3 := 9, 7, 8
+
+	if quiz1 > quiz2 {
+		fmt.Println("Quiz1 second higher than quiz2")
+	} else if quiz1 < quiz2 {
+		fmt.Println("Quiz2 second higher than quiz1")
+	} else {
+		fmt.Println("Quize 1 & Quiz 2 have that same score")
+	}
+
+	if average(quiz1, quiz2, quiz3) > 7 {
+		fmt.Println("acceptable grades")
+	} else {
+		fmt.Println("instructor did a bad job!")
+	}
 }
