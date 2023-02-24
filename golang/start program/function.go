86b1e7e969d6e0fb99ddf51ba103796d1134f0b5
codergalib20 <coderboygalif@gmail.com>
2023-02-24 22:00:18 +0600

    learn multiple parameter and string return function

--- golang/start program/function.go
+++ golang/start program/function.go
@@ -6,6 +6,17 @@ func double(x int) int {
 	return x * x
 }
 
+func doubleDouble(x, y int) int {
+	return (x * y) + 20
+}
+
+func stringReturn(x string) string {
+	return x
+}
+
 func main() {
 	fmt.Println(double(20))
+	var number = doubleDouble(5, 10)
+	fmt.Println(number)
+	fmt.Println(stringReturn("Welcome to string function"))
 }
