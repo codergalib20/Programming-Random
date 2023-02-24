8d5a96d186bf174fc383f2f969a0bf4e284db0db
codergalib20 <coderboygalif@gmail.com>
2023-02-24 15:39:30 +0600

    learn user input

--- golang/start program/const.go
+++ golang/start program/const.go
@@ -3,8 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	const NAME = "Asadullah"
+	var NAME string = "Asadullah"
 	const CLASS_NAME = "Galib"
-
+	fmt.Scan(&NAME)
+	fmt.Println(NAME)
 	fmt.Printf("%v %v", NAME, CLASS_NAME)
 }
