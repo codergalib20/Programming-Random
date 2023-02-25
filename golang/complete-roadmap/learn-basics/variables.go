4de00faf6fe3f5ec0a1fe994478dd0d4fc2e97c6
codergalib20 <coderboygalif@gmail.com>
2023-02-25 20:20:14 +0600

    commited

---
+++ golang/complete-roadmap/learn-basics/variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Hello"
+	age := 18
+	className := true
+
+	var isTrue bool = false
+	var firstName string = "Asadullah"
+
+	fmt.Println(name, " ", age, " ", className, " ", isTrue, " ", firstName)
+}
