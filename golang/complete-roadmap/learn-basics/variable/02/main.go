96d629cc1b65ff1b3066a1a7618d53788f7ab422
codergalib20 <coderboygalif@gmail.com>
2023-02-27 23:59:54 +0600

    commited

--- golang/complete-roadmap/learn-basics/variable/02/main.go
+++ golang/complete-roadmap/learn-basics/variable/02/main.go
@@ -9,4 +9,8 @@ func main() {
 	var username string = "Asadullah"
 	fmt.Println(username)
 	fmt.Printf("Variable is of type : %T \n", username)
+
+	var isLoggedIn bool = false
+	fmt.Printf("Variable is of type : %T \n", isLoggedIn)
+
 }
