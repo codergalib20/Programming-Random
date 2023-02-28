e340e7a5890011dd7e14d558eeb266649607e1bc
codergalib20 <coderboygalif@gmail.com>
2023-02-28 19:50:58 +0600

    learning array slice amd maps in Golang

--- golang/complete-roadmap/learn-basics/array/main.go
+++ golang/complete-roadmap/learn-basics/array/main.go
@@ -6,5 +6,5 @@ func main() {
 	var grades [3]int = [3]int{10, 20, 30}
 	students := [3]int{2, 3, 4}
 	teachers := [3]string{"Galib", "Afnan", "Lamia"}
-	fmt.Println(grades, students, teachers)
+	fmt.Println(grades, students, teachers[2])
 }
