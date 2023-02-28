86142ed7076214eca649bfdfa05da666f8170d99
codergalib20 <coderboygalif@gmail.com>
2023-02-28 20:06:20 +0600

    learning array slice amd maps in Golang

--- golang/complete-roadmap/learn-basics/array/main.go
+++ golang/complete-roadmap/learn-basics/array/main.go
@@ -8,4 +8,13 @@ func main() {
 	teachers := [3]string{"Atiur", "Proshanto", "Ajijar"}
 	froots := [...]int{22, 3, 45, 4, 66, 4, 3}
 	fmt.Println(grades, students, teachers[2], froots)
+	for i := 0; i < len(grades); i++ {
+		fmt.Println(grades[i])
+	}
+	for i := 0; i < len(teachers); i++ {
+		fmt.Println(teachers[i])
+	}
+	for i := 0; i < len(froots); i++ {
+		fmt.Println(froots[i])
+	}
 }
