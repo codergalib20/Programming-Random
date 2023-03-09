5c01853d2a39521443b6ba2b84d535ecb0df5fc5
codergalib20 <coderboygalif@gmail.com>
2023-03-09 19:58:59 +0600

    commited

--- golang/complete-roadmap/learn-basics/array/practice2/main.go
+++ golang/complete-roadmap/learn-basics/array/practice2/main.go
@@ -10,8 +10,14 @@ func main() {
 	// var arr2 []string = []string{"sdfsd", "sdfsd", "sdfsd", "Sdfsd", "sdfsd"}
 	// fmt.Println(arr2)
 
-	arr := []int{3, 43, 4, 3, 43, 43}
-	arr2 := []string{"34", "343", "343", "343"}
+	// 	arr := []int{3, 43, 4, 3, 43, 43}
+	// 	arr2 := []string{"34", "343", "343", "343"}
 
-	fmt.Println(arr, "-", arr2)
+	// 	fmt.Println(arr, "-", arr2)
+
+	var grades [5]int = [5]int{3, 4, 5, 6, 4}
+	for i := 0; i < len(grades); i++ {
+		fmt.Println(grades[i])
+	}
+
 }
