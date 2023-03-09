c34ca67fe0067fae59e973659cde7010b0c8d0e3
codergalib20 <coderboygalif@gmail.com>
2023-03-09 20:00:52 +0600

    commited

--- golang/complete-roadmap/learn-basics/array/practice2/main.go
+++ golang/complete-roadmap/learn-basics/array/practice2/main.go
@@ -16,8 +16,11 @@ func main() {
 	// 	fmt.Println(arr, "-", arr2)
 
 	var grades [5]int = [5]int{3, 4, 5, 6, 4}
-	for i := 0; i < len(grades); i++ {
-		fmt.Println(grades[i])
-	}
+	// for i := 0; i < len(grades); i++ {
+	// 	fmt.Println(grades[i])
+	// }
 
+	for index, element := range grades {
+		fmt.Println(index, " => ", element)
+	}
 }
