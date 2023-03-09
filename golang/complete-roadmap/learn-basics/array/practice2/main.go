d2146010b8e04f7083be0045c7d8feaa2dea73a3
codergalib20 <coderboygalif@gmail.com>
2023-03-09 19:22:03 +0600

    commited

--- golang/complete-roadmap/learn-basics/array/practice2/main.go
+++ golang/complete-roadmap/learn-basics/array/practice2/main.go
@@ -4,8 +4,10 @@ import "fmt"
 
 func main() {
 
-	var arr [5]int
-
+	var arr [5]int = [5]int{23, 4, 5, 566, 6}
 	fmt.Println(arr)
+
+	var arr2 []string = []string{"sdfsd", "sdfsd", "sdfsd", "Sdfsd", "sdfsd"}
+	fmt.Println(arr2)
 
 }
