cd333e2b5e7a22866f319046e1d079edbe820ff8
codergalib20 <coderboygalif@gmail.com>
2023-03-13 21:32:19 +0600

    learning pointer in golang

--- golang/complete-roadmap/learn-basics/pointers/main.go
+++ golang/complete-roadmap/learn-basics/pointers/main.go
@@ -3,16 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	s := "Hello!"
-	var pointer = &s
-	pointer2 := &s
+	// s := "Hello!"
+	// var pointer = &s
+	// pointer2 := &s
 
-	fmt.Println(&pointer)
-	fmt.Println(pointer)
-	fmt.Println(*pointer)
+	// fmt.Println(&pointer)
+	// fmt.Println(pointer)
+	// fmt.Println(*pointer)
 
-	// ----------
-	fmt.Println(pointer2)
-	fmt.Println(&pointer2)
-	fmt.Println(*pointer2)
+	// // ----------
+	// fmt.Println(pointer2)
+	// fmt.Println(&pointer2)
+	// fmt.Println(*pointer2)
+
+	s := "hello"
+
+	var b *string = &s
+	fmt.Println(b)
+	var a = &s
+	fmt.Println(a)
+	c := &s
+	fmt.Println(c)
 }
